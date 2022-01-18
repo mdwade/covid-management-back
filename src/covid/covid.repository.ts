2d89a1c02f2cfdaa34efd79aa78faa1c092e528d
mdwade <37298278+mdwade@users.noreply.github.com>
2022-01-18 09:20:00 +0000

    finish project

--- src/covid/covid.repository.ts
+++ src/covid/covid.repository.ts
@@ -30,23 +30,23 @@ export class CovidRepository implements OnModuleInit {
        return (await this.client.execute(query)).rows;
     }
 
-    async getQII1 (location: any, startedDate: any, endedDate: any) {
-       const query = "SELECT date, total_cases where location = ? and date >= ? and date <= ? ALLOW FILTERING";
-       return (await this.client.execute(query, [location, startedDate, endedDate], {prepare: true})).rows;
-    }
+   async getQII1 (iso_code: string, start_date: string, end_date: string ) {
+      const query = 'SELECT iso_code,date, total_cases,total_deaths,new_cases, new_deaths  FROM covid_by_iso_code_date WHERE iso_code = ? AND date >= ? AND date <= ? GROUP BY date';
+      return (await this.client.execute(query, [ iso_code, start_date, end_date ], { prepare: true })).rows;
+   }
+  
+   async getQII2 (iso_code: string, start_date: string, end_date: string ) {
+      const query = 'SELECT iso_code,date,total_deaths FROM covid_by_iso_code_date WHERE iso_code = ? AND date >= ? AND date <= ? GROUP BY date';
+      return (await this.client.execute(query, [ iso_code, start_date, end_date ], { prepare: true })).rows;
+   }
 
-    async getQII2 (location: any, startedDate: any, endedDate: any) {
-       const query = "SELECT date, new_deaths where location = ? and date >= ? and date <= ?  ALLOW FILTERING";
-       return (await this.client.execute(query, [location, startedDate, endedDate], {prepare: true})).rows;
-    }
+   async getQII3 (date: any) {
+      const query = "SELECT continent, total_cases FROM coviddata_by_continent where date = ? group by continent ALLOW FILTERING";
+      return (await this.client.execute(query, [date], {prepare: true})).rows;
+   }
 
-    async getQII3 (date: any) {
-       const query = "SELECT continent, total_cases FROM coviddata_by_continent where date = ? group by continent ALLOW FILTERING";
-       return (await this.client.execute(query, [date], {prepare: true})).rows;
-    }
-
-    async getQII4 (location: any) {
-        const query = "SELECT date, total_vaccinations, people_vaccinated, people_fully_vaccinated, new_vaccinations, total_vaccinations_per_hundred, people_vaccinated_per_hundred, people_fully_vaccinated_per_hundred, MAX(new_cases) as max_new_cases FROM coviddata_by_location WHERE location = ?";
-        return (await this.client.execute(query, [location], {prepare: true})).rows;
-    }
+   async getQII4 (location: any) {
+      const query = "SELECT date, total_vaccinations, people_vaccinated, people_fully_vaccinated, new_vaccinations, total_vaccinations_per_hundred, people_vaccinated_per_hundred, people_fully_vaccinated_per_hundred, MAX(new_cases) as max_new_cases FROM coviddata_by_location WHERE location = ?";
+      return (await this.client.execute(query, [location], {prepare: true})).rows;
+   }
 }
